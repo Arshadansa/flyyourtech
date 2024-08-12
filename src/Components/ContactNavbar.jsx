155936d87a6acf28afd42dec775fbdc8d4bf7ae9
atif <officialarshad16@gmail.com>
2024-08-12 18:38:28 +0530

    update-fun

--- src/Components/ContactNavbar.jsx
+++ src/Components/ContactNavbar.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoLocationOutline, IoTimeOutline } from "react-icons/io5";
 import { MdOutlinePhone } from "react-icons/md";
 
 function ContactNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav class=" z-20 relative ">
-      <div class="max-w-screen-2xl lg:px-12 pt-6 flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <span class="self-center uppercase text-2xl font-semibold whitespace-nowrap  text-white  ">
+    <nav className="z-20 relative">
+      <div className="max-w-screen-2xl lg:px-12 pt-6 flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center uppercase text-2xl font-semibold whitespace-nowrap text-white">
             Fly Your Tech
           </span>
         </a>
         <button
           data-collapse-toggle="navbar-solid-bg"
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-solid-bg"
-          aria-expanded="false"
+          aria-expanded={isOpen}
+          onClick={toggleMenu}
         >
-          <span class="sr-only">Open main menu</span>
+          <span className="sr-only">Open main menu</span>
           <svg
-            class="w-5 h-5"
+            className="w-5 h-5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -28,19 +35,24 @@ function ContactNavbar() {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
         </button>
-        <div class="hidden w-full lg:block md:w-auto" id="navbar-solid-bg">
-          <ul class="flex flex-col font-medium mt-4 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li className=" flex  items-center lg:border-r-2 pr-12">
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } w-full lg:block md:w-auto`}
+          id="navbar-solid-bg"
+        >
+          <ul className="flex flex-col font-medium mt-4 md:mt-10 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li className="flex items-center lg:border-r-2 pr-12">
               <a
                 href="/"
-                class="block py-2 px-3 md:p-0 text-white  rounded md:bg-transparent 0 md:dark:bg-transparent"
+                className="block py-2 px-3 md:p-0 text-white rounded md:bg-transparent md:dark:bg-transparent"
                 aria-current="page"
               >
                 Home
@@ -49,28 +61,26 @@ function ContactNavbar() {
             <li className="flex gap-2 items-center lg:border-r-2 pr-12">
               <IoTimeOutline size={40} className="text-white mt-2" />
               <div className="flex text-white flex-col">
-                <span class="block py-2 px-3 md:p-0  rounded  ">
-                  Time
-                </span>
-                <span className=" font-extrabold ">10AM - 11:30PM</span>
+                <span className="block py-2 px-3 md:p-0 rounded">Time</span>
+                <span className="font-extrabold">10AM - 11:30PM</span>
               </div>
             </li>
             <li className="flex gap-2 text-white items-center lg:border-r-2 pr-12">
               <IoLocationOutline size={40} className="text-white mt-2" />
               <div className="flex flex-col">
-                <span class="block py-2 px-3 md:p-0 text-white rounded  ">
+                <span className="block py-2 px-3 md:p-0 text-white rounded">
                   Location
                 </span>
-                <span className=" font-extrabold ">(M.P) , INDIA</span>
+                <span className="font-extrabold">(M.P), INDIA</span>
               </div>
             </li>
             <li className="flex gap-2 text-white items-center">
               <MdOutlinePhone size={40} className="text-white mt-2" />
               <div className="flex flex-col">
-                <span class="block py-2 px-3 md:p-0 text-white rounded  ">
-                  Time
+                <span className="block py-2 px-3 md:p-0 text-white rounded">
+                  Phone
                 </span>
-                <span className=" font-extrabold ">+91 747 706 6373</span>
+                <span className="font-extrabold">+91 747 706 6373</span>
               </div>
             </li>
           </ul>
